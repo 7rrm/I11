e702fe95064690196e6ca687ceb435806048ab3b
7rrm <krardh17@gmail.com>
2026-08-19 14:14:09 +0300

    Update MeeroUsageGuide.java

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/MeeroUsageGuide.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/MeeroUsageGuide.java
@@ -83,9 +83,9 @@ public final class MeeroUsageGuide {
         
         // Padding صغير للزر البيضاوي الرفيع
         button.setPadding(
-                AndroidUtilities.dp(80),
+                AndroidUtilities.dp(100),
                 AndroidUtilities.dp(10),
-                AndroidUtilities.dp(80),
+                AndroidUtilities.dp(100),
                 AndroidUtilities.dp(10)
         );
 
