695ed38b27a9b360ea09e0b36ac2a093ba51ef30
7rrm <krardh17@gmail.com>
2026-08-22 12:54:42 +0300

    Update ApplicationLoader.java

--- TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
@@ -399,7 +399,6 @@ public class ApplicationLoader extends Application {
         // The master switch defaults OFF, so start() runs a cheap check that
         // exits immediately for users who never armed it.
         tw.nekomimi.nekogram.MeeroJanitor.start();
-        tw.nekomimi.nekogram.MeeroTagHunter.start();
     }
 
     // Local Push Service, TFoss implementation
