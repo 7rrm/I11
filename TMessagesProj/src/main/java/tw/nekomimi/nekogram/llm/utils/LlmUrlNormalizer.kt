2fbecd93d2b58395ca072596a2607c40d90fce21
7rrm <krardh17@gmail.com>
2026-08-22 16:04:01 +0300

    Rename UrlNormalizer to LlmUrlNormalizer

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/llm/utils/LlmUrlNormalizer.kt
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/llm/utils/LlmUrlNormalizer.kt
@@ -2,7 +2,7 @@ package tw.nekomimi.nekogram.llm.utils
 
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 
-object UrlNormalizer {
+object LlmUrlNormalizer {
 
     private const val HTTP_PREFIX = "http://"
     private const val HTTPS_PREFIX = "https://"
